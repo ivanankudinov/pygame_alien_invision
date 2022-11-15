76632052facad8746db4be310997a80a09fd3da1
Ivan Ankudinov <ivan.ankudinov97@gmail.com>
2022-11-15 16:37:30 +0300

    First commit of game. Aliens comming down to the ship. Ship can move right and left. Shit can shoot.

---
+++ Pygame.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Unit:
+
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1200, 800))
+
+    def run(self):
+
+
+if __name__ =='__main__':
+    game = Unit()
+    game.run()
